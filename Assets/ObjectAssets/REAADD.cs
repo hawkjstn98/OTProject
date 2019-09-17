602d8b05f04e81d3f74e2ffdfe6a6ac57c6aed73
Justin Susanto <39823674+hawkjstn98@users.noreply.github.com>
2019-09-17 18:43:16 +0700

    Add README and ObjectAsset File

---
+++ Assets/ObjectAssets/REAADD.cs
@@ -0,0 +1,3 @@
+﻿// This Folder Is Only for Assets, Everyone must put Image Assets, BGM, etc HERE
+// For License and copyrights, this folder will be ignored with git.
+// If you want to add assets, pls add it manually
